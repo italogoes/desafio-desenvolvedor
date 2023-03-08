fa7446b056accf85f6e03c258298e1eeaa6739dd
Ítalo Góes <italogoes85@gmail.com>
2023-03-08 16:15:01 -0300

    projeto finalizado

--- desafio-catus/assets/js/index.js
+++ desafio-catus/assets/js/index.js
@@ -6,4 +6,28 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        240: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      },
 });
+
+function abrirMenu(){
+    let menu = document.querySelector('.menu-mobile');
+    menu.style.display = 'block'
+}
+
+function fecharMenu(){
+    let menu = document.querySelector('.menu-mobile');
+    menu.style.display = 'none'
+}
